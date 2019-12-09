84adbb0684a6d7a2c1b2dc8189e35d7a77800d21
Philip <p208p2002@gmail.com>
2019-12-09 03:08:05 +0000

    tensor dataset

--- core.py
+++ core.py
@@ -1,5 +1,32 @@
 import csv
 from transformers import AlbertTokenizer
+import torch
+from torch.utils.data import TensorDataset
+
+def makeTorchDataSet(mr_data_class,is_train_data = True):
+    all_input_ids = []
+    all_answer_lables = []
+    max_input_length = 0
+    for d in mr_data_class.data:
+        input_ids = d[-1]
+        if(len(input_ids)>max_input_length):
+            max_input_length = len(input_ids)
+        all_input_ids.append(input_ids)
+        if(is_train_data):
+            Sentiment = d[-2]
+            all_answer_lables.append(int(Sentiment))
+    
+    for input_ids in all_input_ids:
+        while(len(input_ids)<max_input_length):
+            input_ids.append(0)
+        assert len(input_ids) == max_input_length
+
+    if(is_train_data):
+        torch_input_ids = torch.tensor([ids for ids in all_input_ids], dtype=torch.long)
+        torch_answer_lables = torch.tensor([answer_lable for answer_lable in all_answer_lables], dtype=torch.long)
+        return TensorDataset(torch_input_ids,torch_answer_lables)
+
+    return TensorDataset(torch_input_ids)
 
 class MR_Data:
     def __init__(self, is_train_data, data):
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
-from core import MR_Data
+from core import MR_Data,makeTorchDataSet
 if __name__ == "__main__":
     TrainData = MR_Data.load_data('dataset/train.tsv')
+    makeTorchDataSet(TrainData)
