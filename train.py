9cf75854b143e5fda93efbcfea5ac4f3cdd77ab5
Philip <p208p2002@gmail.com>
2019-12-09 03:47:23 +0000

    no message

--- train.py
+++ train.py
@@ -6,3 +6,10 @@ if __name__ == "__main__":
     TrainDataLoader = makeTorchDataLoader(TrainDataset)
     model_config = AlbertConfig.from_json_file('albert-large-config.json')
     model = AlbertForSequenceClassification.from_pretrained('albert-large-pytorch_model.bin',config = model_config)
+
+    # tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+    # model = AlbertForSequenceClassification.from_pretrained('albert-base-v2')
+    # input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+    # labels = torch.tensor([1]).unsqueeze(0)  # Batch size 1
+    # outputs = model(input_ids, labels=labels)
+    # loss, logits = outputs[:2]
