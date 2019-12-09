3c50537ebb0239493d76581208a017c14257e8d5
Philip <p208p2002@gmail.com>
2019-12-09 03:36:18 +0000

    no message

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sentiment-analysis-on-movie-reviews
 [Kaggle](https://www.kaggle.com/c/sentiment-analysis-on-movie-reviews/data)
+
+## model file
+albert large
+
+- [vocab](https://s3.amazonaws.com/models.huggingface.co/bert/albert-large-spiece.model)
+- [config_file](https://s3.amazonaws.com/models.huggingface.co/bert/albert-large-config.json)
+- [model](https://s3.amazonaws.com/models.huggingface.co/bert/albert-large-pytorch_model.bin)
--- train.py
+++ train.py
@@ -1,5 +1,8 @@
 from core import MR_Data, makeTorchDataSet, makeTorchDataLoader
+from transformers import AlbertConfig,AlbertForSequenceClassification
 if __name__ == "__main__":
     TrainData = MR_Data.load_data('dataset/train.tsv')
     TrainDataset = makeTorchDataSet(TrainData)
     TrainDataLoader = makeTorchDataLoader(TrainDataset)
+    model_config = AlbertConfig.from_json_file('albert-large-config.json')
+    model = AlbertForSequenceClassification.from_pretrained('albert-large-pytorch_model.bin')
