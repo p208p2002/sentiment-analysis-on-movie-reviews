f1815b741c722d07988d27e23f140c897a3ff717
Philip <p208p2002@gmail.com>
2019-12-11 02:17:30 +0000

    test data

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ def main():
     FullDataset = makeTorchDataSet(FullData)
     TrainDataset, TestDataset =  splitDataset(FullDataset, 0.9)
     TrainDataLoader = makeTorchDataLoader(TrainDataset, batch_size=16)
+    TestDataLoader = makeTorchDataLoader(TestDataset, batch_size = 8)
     model_config = AlbertConfig.from_json_file('model/albert-large-config.json')
     model = AlbertForSequenceClassification.from_pretrained('model/albert-large-pytorch_model.bin', config = model_config)
     model.to(device)
@@ -27,6 +28,7 @@ def main():
 
     try:
         for epoch in range(15):
+            # train
             running_loss_val = 0.0
             running_acc = 0.0
             for batch_index, batch_dict in enumerate(TrainDataLoader):
@@ -48,8 +50,28 @@ def main():
 
                 # log
                 if(batch_index % 50 == 0):
-                    log("epoch:%2d batch:%4d train_loss:%2.4f train_acc:%3.4f"%(epoch+1, batch_index+1, running_loss_val, running_acc))
-        
+                    log(">> TRAIN << epoch:%2d batch:%4d loss:%2.4f acc:%3.4f"%(epoch+1, batch_index+1, running_loss_val, running_acc))
+            
+            # test
+            running_loss_val = 0.0
+            running_acc = 0.0
+            for batch_index, batch_dict in enumerate(TestDataLoader):
+                model.eval()
+                batch_dict = tuple(t.to(device) for t in batch_dict)
+                outputs = model(batch_dict[0], labels=batch_dict[1])
+                loss, logits = outputs[:2]
+
+                # compute the loss
+                loss_t = loss.item()
+                running_loss_val += (loss_t - running_loss_val) / (batch_index + 1)
+
+                # compute the accuracy
+                acc_t = computeAccuracy(logits, batch_dict[1])
+                running_acc += (acc_t - running_acc) / (batch_index + 1)
+
+                # log
+                log(">> TEST << epoch:%2d batch:%4d loss:%2.4f acc:%3.4f"%(epoch+1, batch_index+1, running_loss_val, running_acc))
+                    
             # save model
             saveModel(model,'ALSS_e%s_a%s'%(str(epoch+1),str(running_acc)))
     
