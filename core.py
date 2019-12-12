dba1dd74aa4246608b247d5f6b6e47d99f6df2db
Philip <p208p2002@gmail.com>
2019-12-12 09:18:27 +0000

    no message

--- core.py
+++ core.py
@@ -37,12 +37,14 @@ def makeTorchDataLoader(torch_dataset,batch_size = 16):
 def makeTorchDataSet(mr_data_class,is_train_data = True):
     all_input_ids = []
     all_answer_lables = []
+    all_input_phrase_ids = []
     max_input_length = 0
     for d in mr_data_class.data:
         input_ids = d[-1]
         if(len(input_ids)>max_input_length):
             max_input_length = len(input_ids)
         all_input_ids.append(input_ids)
+        all_input_phrase_ids.append(d[0])
         if(is_train_data):
             Sentiment = d[-2]
             all_answer_lables.append(int(Sentiment))
@@ -56,8 +58,10 @@ def makeTorchDataSet(mr_data_class,is_train_data = True):
         torch_input_ids = torch.tensor([ids for ids in all_input_ids], dtype=torch.long)
         torch_answer_lables = torch.tensor([answer_lable for answer_lable in all_answer_lables], dtype=torch.long)
         return TensorDataset(torch_input_ids,torch_answer_lables)
-
-    return TensorDataset(torch_input_ids)
+    else:
+        torch_input_ids = torch.tensor([ids for ids in all_input_ids], dtype=torch.long)
+        torch_phrase_ids = torch.tensor([int(ids) for ids in all_input_phrase_ids], dtype=torch.long)
+        return TensorDataset(torch_input_ids,torch_phrase_ids)
 
 def splitDataset(full_dataset,split_rate = 0.8):
     train_size = int(split_rate * len(full_dataset))
---
+++ predict_test.py
@@ -0,0 +1,47 @@
+import torch
+from core import MR_Data, makeTorchDataSet, makeTorchDataLoader, blockPrint, enablePrint, log, computeAccuracy, saveModel, splitDataset
+from transformers import AlbertConfig, AlbertForSequenceClassification, AdamW
+import numpy as np
+
+def main():
+    #
+    blockPrint()
+
+    # setting device
+    device = torch.device('cuda')
+
+    #
+    FullData = MR_Data.load_data('dataset/test.tsv',is_train_data=False)
+    FullDataset = makeTorchDataSet(FullData,is_train_data=False)
+    TestDataLoader = makeTorchDataLoader(FullDataset, batch_size=16)
+    model_config = AlbertConfig.from_json_file('model/albert-large-config.json')
+    trained_model_file = '12-11-2019_09-17-05_ALSS_e5_a69.24226892192033'
+    model = AlbertForSequenceClassification.from_pretrained('train_models/'+ trained_model_file +'/pytorch_model.bin', config = model_config)
+    
+    model.to(device)
+    model.eval()
+
+    f = open('submission.csv','w',encoding='utf-8')
+    f.write('PhraseId,Sentiment\n')
+
+    for batch_index, batch_dict in enumerate(TestDataLoader):
+        batch_dict = tuple(t.to(device) for t in batch_dict)
+        input_ids,phrase_ids = batch_dict
+        outputs = model(input_ids)
+        
+        outputs = outputs[0].cpu()
+        outputs = outputs.detach().numpy()
+        # log(outputs)
+        
+        for i in range(len(batch_dict)):
+            p_id = phrase_ids[i].item()
+            s_level = np.argmax(outputs[i])
+            log("phrase_id",p_id,"segment_level",s_level)
+            f.write(str(p_id) + ',' + str(s_level) + '\n')
+    
+    f.close()
+
+if __name__ == '__main__':
+    main()
+    
+    
